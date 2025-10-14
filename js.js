90bd418443aa4c3b36a0d274b3abb30bba951368
asomaarooufiniyaa <asomaarooufiniyaa@gmail.com>
2025-10-14 06:41:51 +0330

    js file updated

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world of techs!');
 });
 
 app.get('/secret', (req, res) => {
